ae816650df1ab4984f031d29e16200b2aedd9927
iamangelu <67723309+iamangelu@users.noreply.github.com>
2020-09-20 12:47:04 +0800

    Updates

---
+++ precache-manifest.665a7acf32a5a221cdf6aee133a75abb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2878a44daa2d82b079d047d46ea6de6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3df52642b14bddd8f70c",
+    "url": "/static/css/main.11d54a90.chunk.css"
+  },
+  {
+    "revision": "eb92d983cae4b40cf08d",
+    "url": "/static/js/2.f141c0b0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f141c0b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3df52642b14bddd8f70c",
+    "url": "/static/js/main.fa54ee57.chunk.js"
+  },
+  {
+    "revision": "2048b6c46f34851a1713",
+    "url": "/static/js/runtime-main.d4a21cb6.js"
+  }
+]);
